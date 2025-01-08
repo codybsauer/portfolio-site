18f491fe918cceac4a7bcf0255fe6b02de1480b0
Cody Sauer <cody.b.sauer@gmail.com>
2025-01-07 16:58:15 -0800

    Adjust high score calculation.

--- src/app/components/Navigation/HighScoreCounter.tsx
+++ src/app/components/Navigation/HighScoreCounter.tsx
@@ -5,8 +5,21 @@ export const HighScoreCounter = () => {
   const [highScore, setHighScore] = useState("Loading score...");
 
   useEffect(() => {
+    const calculateHighScore = () => {
+      const then = 648806400000;
+      const now = new Date().getTime();
+      const diff = now - then;
+
+      const seconds = Math.floor((diff / 1000) % 60);
+      const minutes = Math.floor((diff / 1000 / 60) % 60);
+      const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
+      const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+
+      return `${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`;
+    };
+
     const interval = setInterval(() => {
-      setHighScore(Date.now().toString());
+      setHighScore(calculateHighScore());
     }, 1000);
 
     return () => clearInterval(interval);
