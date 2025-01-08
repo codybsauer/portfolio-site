379d00c8141261b05544ca6046133b2df969dfb1
Cody Sauer <cody.b.sauer@gmail.com>
2025-01-07 17:01:25 -0800

    One more fix.

--- src/app/components/Navigation/HighScoreCounter.tsx
+++ src/app/components/Navigation/HighScoreCounter.tsx
@@ -10,12 +10,7 @@ export const HighScoreCounter = () => {
       const now = new Date().getTime();
       const diff = now - then;
 
-      const seconds = Math.floor((diff / 1000) % 60);
-      const minutes = Math.floor((diff / 1000 / 60) % 60);
-      const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
-      const days = Math.floor(diff / (1000 * 60 * 60 * 24));
-
-      return `${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`;
+      return diff.toString();
     };
 
     const interval = setInterval(() => {
