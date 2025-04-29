0e83d200c934a9a6ceff1fe3beaf81cc660b2d38
Cody Sauer <cody.b.sauer@gmail.com>
2025-04-29 11:28:01 -0700

    Hobbies update.

--- src/app/components/Hobbies/constants.ts
+++ src/app/components/Hobbies/constants.ts
@@ -18,18 +18,6 @@ export const hobbies: HobbyCategory[] = [
           "GMing for a party of 3",
         ],
       },
-      {
-        title: "Pathfinder 2e - Lyra, Spice, and Everything Nice",
-        status: "RUNNING",
-        description:
-          "Welcome to the Deep Wilds! An outer plane overgrown with natural splendor. Your memories have been stolen and dark forces move to subjugate this land. Recover your memories and stop them before it's too late!",
-        details: [
-          "Foundry VTT",
-          "Discord",
-          "GMing for an international party of 3",
-          "Returning characters from the wider Codyverse",
-        ],
-      },
     ],
   },
   {
@@ -37,113 +25,30 @@ export const hobbies: HobbyCategory[] = [
     icon: "📚",
     entries: [
       {
-        title: "Wind and Truth",
+        title:
+          "Adult Children of Emotionally Immature Parents: How to Heal from Distant, Rejecting, or Self-Involved Parents",
         status: "READING",
-        description: "Book 5 of the Stormlight Archive epic fantasy series",
-        details: [
-          "Author: Brandon Sanderson",
-          "It's going to be so long before we get another book!",
-        ],
+        description:
+          "A self help book I've been reading since my trip to the mountains. It's really helping me process a lot.",
+        details: ["Author: Lindsay C. Gibson"],
       },
     ],
   },
   {
-    title: "TV SHOWS",
-    icon: "📺",
-    entries: [
-      {
-        title: "The Apothecary Diaries",
-        status: "WATCHING",
-        description:
-          "Season 2 of the anime adaptation following a curious apothecary embroiled in palatial intrigue",
-        details: ["Spring 2025 Season", "Maomao is so cute!"],
-      },
-      {
-        title: "Fringe",
-        status: "WATCHING",
-        description:
-          "Older TV Show that hits all the right crime drama and sci-fi elements I enjoy",
-        details: ["Walter is somehow the best and the worst"],
-      },
-    ],
-  },
-  {
-    title: "MOVIES",
-    icon: "🎬",
-    entries: [
-      {
-        title: "Sonic the Hedgehog 3",
-        status: "WATCHED",
-        description: "Edgy boi Shadow via Keanu Reeves",
-        details: [
-          "Binged all three of these movies with my friends. Unexpectedly great!",
-        ],
-      },
-      {
-        title: "Red One",
-        status: "WATCHED",
-        description:
-          "Holiday action-adventure film starring Dwayne 'The Rock' Johnson and Chris Evans",
-        details: [
-          "More entertaining than it has any right to be. Especially with the appropriate amount of holiday 'cheer'",
-        ],
-      },
-    ],
-  },
-  {
-    title: "SINGLE-PLAYER VIDEO GAMES",
+    title: "VIDEO GAMES",
     icon: "🕹️",
     entries: [
       {
-        title: "The Legend of Heroes: Trails through Daybreak",
-        status: "REPLAY",
-        description:
-          "The 11th game in the epic fantasy JRPG series. This one is set in the nation of Calvard",
-        details: [
-          "Developer: Nihon Falcom",
-          "Doing an abridged cleanup playthrough to get anything I missed before 2 releases",
-          "Long time favorite",
-          "Nothing else quite like it in the video game space",
-        ],
+        title: "Clair Obscur: Expedition 33",
+        status: "PLAYING SOON",
+        description: "A FRPG?",
+        details: ["Developer: Sandfall Interactive"],
       },
       {
-        title: "The Legend of Heroes: Trails through Daybreak 2",
-        status: "ANTICIPATING",
-        description:
-          "12th game in the series bringing back many favorite characters and expanding on their stories",
-        details: [
-          "Release date: 2/14/2025",
-          "Developer: Nihon Falcom",
-          "I get to play as Renne again. Yay!",
-        ],
-      },
-    ],
-  },
-  {
-    title: "MULTIPLAYER VIDEO GAMES",
-    icon: "🎮",
-    entries: [
-      {
-        title: "Marvel Rivals",
-        status: "HIATUS",
-        description:
-          "IT'S SPIDER TIME!!! Marvel hero shooter. Great casual game to play with friends",
-        details: [
-          "Main/Vanguard: Peni Parker",
-          "Tactician: Luna Snow",
-          "Duelist: Moon Knight",
-        ],
-      },
-      {
-        title: "The Finals",
-        status: "HIATUS",
-        description:
-          "Performance issues aside, I dig the destructability of levels and creative approaches this game allows for",
-        details: [
-          "Medium: Support",
-          "Heavy: Spin to win!",
-          "Current Sponsor: Dissun",
-        ],
+        title: "Natsu-Mon: 20th Century Summer Kid",
+        status: "PLAYING SOON",
+        description: "I plan to play this in Japanese for learning purposes.",
+        details: ["Developers: Millennium Kitchen, TOYBOX Inc."],
       },
     ],
   },
